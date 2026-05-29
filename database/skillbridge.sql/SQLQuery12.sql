0a332712136e6a03caecd5f109d374f09c05b979
esrailgin <144242585+esrailgin@users.noreply.github.com>
2026-05-30 00:12:45 +0300

    Delete database/skillbridge.sql directory

--- database/skillbridge.sql/SQLQuery12.sql
+++
@@ -1,9 +0,0 @@
-USE SkillBridge;
-GO
-
-IF COL_LENGTH('dbo.CVAnalyses', 'file_path') IS NULL
-BEGIN
-    ALTER TABLE dbo.CVAnalyses
-    ADD file_path NVARCHAR(1000) NULL;
-END
-GO
--- database/skillbridge.sql/SQLQuery5.sql
+++
@@ -1,5 +0,0 @@
-USE SkillBridge;
-GO
-
-SELECT COUNT(*) AS BeceriSayisi FROM dbo.SkillsInventory;
-SELECT COUNT(*) AS RozetSayisi FROM dbo.Badges;
